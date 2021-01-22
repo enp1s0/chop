f1e67de97bdf729d3acfeb962a4a926c4a64e1fd
mutsuki <mutsuki@momo86.net>
2021-01-22 18:56:50 +0900

    Add include/chopfp/detail/sr_1.hpp

---
+++ include/chopfp/detail/sr_1.hpp
@@ -0,0 +1,43 @@
+#ifndef __MTK_CHOP_DETAIL_SR_1_HPP__
+#define __MTK_CHOP_DETAIL_SR_1_HPP__
+#include <cmath>
+#include "macro.hpp"
+#include "utils.hpp"
+
+namespace mtk {
+namespace chopfp {
+namespace detail {
+template <class T, class RandFunc>
+FUNC_MACRO T chop_sr_1(const T v, const unsigned leaving_length, RandFunc rand_func) {
+	const unsigned shift = detail::get_mantissa_size<T>() - leaving_length;
+	const auto bs_mantissa_0 = detail::mask_mantissa(v);
+	const auto bs_mantissa = bs_mantissa_0 | (decltype(bs_mantissa_0)(1) << detail::get_mantissa_size<T>());
+
+	auto move_up = 0;
+	const auto cut_off = bs_mantissa - ((bs_mantissa >> shift) << shift);
+	const typename mtk::chopfp::detail::same_size_uint<T>::type rand = rand_func(bs_mantissa);
+	const auto threshold = rand - ((rand >> shift) << shift);
+	if (cut_off >= threshold) {
+		move_up = 1;
+	}
+
+	// Move up if necessary
+	const auto result_bs_mantissa_0 = ((bs_mantissa >> shift) + move_up) << shift;
+	const auto correct_mantissa_shift = (result_bs_mantissa_0 >> detail::get_mantissa_size<T>()) >> 1;
+	const auto result_bs_mantissa = result_bs_mantissa_0 >> correct_mantissa_shift;
+
+	const auto bs_exponent = detail::mask_exponent(v) >> detail::get_mantissa_size<T>();
+	const auto result_bs_exponent = bs_exponent + correct_mantissa_shift;
+	if ((result_bs_exponent >> detail::get_exponent_size<T>()) > 0) {
+		return static_cast<T>(v * INFINITY);
+	}
+
+	const auto bs_result = detail::mask_sign(v)
+		| (result_bs_exponent << detail::get_mantissa_size<T>())
+		| (result_bs_mantissa & ((decltype(bs_mantissa_0)(1) << detail::get_mantissa_size<T>()) - 1));
+	return detail::reinterpret_as_fp<typename detail::same_size_uint<T>::type, T>(bs_result);
+}
+} // namespace detail
+} // namesapce chop
+} // namespace mtk
+#endif
